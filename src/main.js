ade6a4cb5f7979ac3b08a5a701944a6e161999db
PengMeng <383867421@qq.com>
2020-04-03 11:11:10 +0800

    加入旋转

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 let demo = document.querySelector("#demo");
 let style = document.querySelector("#demo1");
 let string = `/*您好，我是一名前端工程师
-*下面我要开始装逼了
+*下面我要开始展示我的技术了
 *首先我要准备一个div
 **/
 #div1{
@@ -14,31 +14,44 @@ let string = `/*您好，我是一名前端工程师
 *首先把div变成一个圆圈
 **/
 #div1{
+    position: relative;
     border-radius: 50%;
     box-shadow:0 0 3px rgba(0,0,0,0.5);
     border:none;
   }
   /*八卦是阴阳图
   */
+
  #div1{
     background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
-   }
+    
+  }
 /*魔丸就要诞生了，战斗吧
 */
+#div1{
+  animation: 5s linear infinite  circle;
+}
+@keyframes circle{
+  from{
+      transform : rotate(0deg);
+  }
+  to{
+      transform : rotate(360deg);
+  }
+}
 #div1::before{
     top:0;
-    left:50%;
-    transform: translateX(-50%);
+    left:25%;
     border-radius: 50%;
     background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 50%);
 }
 #div1::after{
     bottom: 0px;
-    left:50%;
-    transform: translateX(-50%);
+    left:25%;
     border-radius: 50%;
     background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 50%, rgba(255,255,255,1) 50%);
-}    
+} 
+   
 `;
 let n = 0;
 //string = string.replace(/\n/g, "<br>");
